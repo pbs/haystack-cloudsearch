ca5932466262f9d96a3b1b4239b1945f4a9fae82
Brandon Adams <emidln@gmail.com>
2012-09-11 15:13:24 -0400

    Apache 2.0 License

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ def read(fname):
 
 setup(
         name='haystack-cloudsearch',
-        version='0.1',
+        version='0.2',
         description='An Amazon Cloudsearch backend for Haystack',
         long_description=read('README.rst'),
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
-            'License :: OSI Approved? :: TBD',
+            'License :: OSI Approved :: Apache Software License',
             'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
             'Framework :: Django',
         ],
         author='Brandon Adams',
         author_email='emidln@gmail.com',
         url='https://github.com/pbs/haystack-cloudsearch',
-        license='TBD',
+        license='Apache License (2.0)',
         py_modules=['haystack_cloudsearch'],
 )
